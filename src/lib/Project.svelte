e1cb197ac9331ac6e79d23eb0568e4edf9e4d096
moralesdaniela <danmor@mit.edu>
2024-03-18 22:51:32 -0400

    Steps 1-3

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -4,9 +4,14 @@ export let hLevel = 2;
 let p = info;
 </script>
 
+
 <article>
         <svelte:element this={"h" + hLevel}>{ p.title }</svelte:element>
 		<img src={p.image} alt="">
 		<p>{p.description}</p>
+		<p class="date">{p.year}</p>
 	</article>
+
+
+
 
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,17 @@
 <script>
+import Pie from '$lib/Pie.svelte'
 import projects from '$lib/projects.json';
 import Project from "$lib/Project.svelte";
+import * as d3 from "d3";
+
+
+let rolledData = d3.rollups(projects, v => v.length, d => d.year);
+
+let pieData = rolledData.map(([year, count]) => {
+	return { value: count, label: year };
+});
 </script>
+<Pie data={pieData} />
 <svelte:head>
 	<title>My Projects</title>
 </svelte:head>
